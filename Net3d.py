9b451f6bc588a6054c233961fa925f09c85ad0fb
GeoMarX <56643703+GeoMarX@users.noreply.github.com>
2022-05-12 01:26:38 +0200

    SpectralConv3 in JAX

---
+++ Net3d.py
@@ -0,0 +1,62 @@
+
+import jax
+import jax.numpy as jnp
+import haiku as hk
+
+
+
+class SpectralConv3(hk.Module):
+  """Attempt"""
+
+  def __init__(self, in_channels=1, out_channels=1, modes1=32, modes2=32, modes3=32, is_training=True, name='Layer'):
+
+    super().__init__(name=name)
+    self.name= name
+    self.in_channels = in_channels
+    self.out_channels = out_channels
+    self.modes1 = modes1 #Number of Fourier modes to multiply, at most floor(N/2) + 1
+    self.modes2 = modes2
+    self.modes3 = modes3
+    
+    self.scale = (1 / (self.in_channels * self.out_channels))
+    self.weights1= hk.get_parameter(str(self.name)+ "w1", shape=[in_channels, out_channels, self.modes1, self.modes2, self.modes3], dtype=float, init=jnp.ones)
+    self.weights2= hk.get_parameter(str(self.name)+ "w2", shape=[in_channels, out_channels, self.modes1, self.modes2, self.modes3], dtype=float, init=jnp.ones)
+    self.weights3= hk.get_parameter(str(self.name)+ "w3", shape=[in_channels, out_channels, self.modes1, self.modes2, self.modes3], dtype=float, init=jnp.ones)
+    self.weights4= hk.get_parameter(str(self.name)+ "w4", shape=[in_channels, out_channels, self.modes1, self.modes2, self.modes3], dtype=float, init=jnp.ones)
+
+
+  def __call__(self, pot_k):
+
+    def compl_mul3d(input, weights):
+        # (batch, in_channel, x,y,t ), (in_channel, out_channel, x,y,t) -> (batch, out_channel, x,y,t)
+        return jnp.einsum("bixyz,ioxyz->boxyz", input, weights)
+    
+  
+        
+    self.weights1=self.weights1*self.scale
+    self.weights2=self.weights2*self.scale
+    self.weights3=self.weights3*self.scale
+    self.weights4=self.weights4*self.scale
+
+    batchsize=1
+    
+    x_ft=pot_k
+    x_ft=x_ft.reshape(1,1,64,64,33)
+
+    out_ft=jnp.zeros([batchsize, self.out_channels, 64, 64, 33], dtype=float)
+
+    out_ft=out_ft.at[:, :, :self.modes1, :self.modes2, :self.modes3]. \
+    set(compl_mul3d(x_ft[:, :, :self.modes1, :self.modes2, :self.modes3], self.weights1))
+
+    out_ft=out_ft.at[:, :, -self.modes1:, :self.modes2, :self.modes3]. \
+    set(compl_mul3d(x_ft[:, :, -self.modes1:, :self.modes2, :self.modes3], self.weights2))
+
+    out_ft=out_ft.at[:, :, :self.modes1, -self.modes2:, :self.modes3]. \
+    set(compl_mul3d(x_ft[:, :, :self.modes1, -self.modes2:, :self.modes3], self.weights3))
+
+    out_ft=out_ft.at[:, :, -self.modes1:, -self.modes2:, :self.modes3]. \
+    set(compl_mul3d(x_ft[:, :, -self.modes1:, -self.modes2:, :self.modes3], self.weights4))
+
+    
+
+    return out_ft.reshape(64,64,33)
